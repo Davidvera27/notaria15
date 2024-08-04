850ecabc3935790ebdb0808649d019d3f37d1b95
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-04 09:35:57 -0500

    Delete iniciar_backend.bat

--- iniciar_backend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Cambiar directorio a la ubicación del backend y ejecutar el servidor
-start "" cmd /c "cd /d \"C:\Users\DAVID\OneDrive - IUE\Desktop\PROGRAMACIÓN\JAVASCRIPT\Notaria 15\notaria15\backend\" && python app.py"
-
-REM Pausar un momento para asegurar que el servidor backend esté completamente iniciado
-timeout /t 5 /nobreak > nul
-
-REM Abrir el navegador predeterminado en la URL de Vercel
-start "" https://notaria15.vercel.app
