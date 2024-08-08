3b7b21c15e9074ae292f8b03bf9529a9ac94c880
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-07 23:02:15 -0500

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,24 @@
+// Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSpring, animated } from '@react-spring/web';
 import './Home.css';
 
 const Home = () => {
+  const fadeIn = useSpring({
+    from: { opacity: 0, transform: 'translate3d(0,-50px,0)' },
+    to: { opacity: 1, transform: 'translate3d(0,0,0)' },
+    config: { duration: 1000 },
+  });
+
   return (
-    <div className="home-container">
+    <animated.div style={fadeIn} className="home-container">
       <h1 className="home-title">Bienvenido a Gestión de Rentas - Notaria 15</h1>
       <div className="home-buttons">
         <Link to="/cases" className="home-button">Gestión de Casos</Link>
         <Link to="/protocolists" className="home-button">Gestión de Protocolistas</Link>
       </div>
-    </div>
+    </animated.div>
   );
 };
 
