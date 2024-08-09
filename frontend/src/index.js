e9229a9d396a0ac9147b919316e068033c1e5b06
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-08 21:20:45 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Estos valores los obtienes de la configuración de tu aplicación en Auth0
+const domain = "dev-lyvcdwdxsmzfi4ng.us.auth0.com";
+const clientId = "eB34oLkIVzW3wxvH2FPS4g08mimfPzHy";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
