c04ee477bde9baca66cac400e024d9d6d1ac3f0f
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-11 17:18:43 -0500

    Update Profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -16,7 +16,6 @@ const Profile = () => {
     username: user.nickname || ''
   });
 
-  // Fetch user profile only if it exists
   const fetchUserProfile = useCallback(async () => {
     try {
       const response = await fetch(`/userprofiles?email=${user.email}`);
@@ -48,36 +47,6 @@ const Profile = () => {
     }
   }, [isAuthenticated, fetchUserProfile]);
 
-  const handleInputChange = (e) => {
-    setUserProfile({ ...userProfile, [e.target.name]: e.target.value });
-  };
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    if (!userProfile.id) {
-      alert('No se puede actualizar el perfil porque el ID es nulo.');
-      return;
-    }
-    try {
-      const response = await fetch(`/userprofiles/${userProfile.id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userProfile),
-      });
-
-      if (response.ok) {
-        alert('¡Perfil actualizado exitosamente!');
-      } else {
-        alert('No se pudo actualizar el perfil');
-      }
-    } catch (error) {
-      console.error('Error actualizando el perfil:', error);
-      alert('No se pudo actualizar el perfil');
-    }
-  };
-
   return (
     isAuthenticated && (
       <div className="profile-container">
@@ -86,36 +55,35 @@ const Profile = () => {
           <h2>{`${userProfile.first_name1} ${userProfile.first_name2} ${userProfile.last_name1} ${userProfile.last_name2}`}</h2>
           <p>{userProfile.email}</p>
         </div>
-        <form onSubmit={handleFormSubmit}>
+        <form>
           <label>
             Nombre 1:
-            <input type="text" name="first_name1" value={userProfile.first_name1} onChange={handleInputChange} />
+            <input type="text" name="first_name1" value={userProfile.first_name1} readOnly />
           </label>
           <label>
             Nombre 2:
-            <input type="text" name="first_name2" value={userProfile.first_name2} onChange={handleInputChange} />
+            <input type="text" name="first_name2" value={userProfile.first_name2} readOnly />
           </label>
           <label>
             Apellido 1:
-            <input type="text" name="last_name1" value={userProfile.last_name1} onChange={handleInputChange} />
+            <input type="text" name="last_name1" value={userProfile.last_name1} readOnly />
           </label>
           <label>
             Apellido 2:
-            <input type="text" name="last_name2" value={userProfile.last_name2} onChange={handleInputChange} />
+            <input type="text" name="last_name2" value={userProfile.last_name2} readOnly />
           </label>
           <label>
             Número telefónico:
-            <input type="text" name="phone_number" value={userProfile.phone_number} onChange={handleInputChange} />
+            <input type="text" name="phone_number" value={userProfile.phone_number} readOnly />
           </label>
           <label>
             Fecha de nacimiento:
-            <input type="date" name="birth_date" value={userProfile.birth_date} onChange={handleInputChange} />
+            <input type="date" name="birth_date" value={userProfile.birth_date} readOnly />
           </label>
           <label>
             Nombre de usuario:
-            <input type="text" name="username" value={userProfile.username} onChange={handleInputChange} />
+            <input type="text" name="username" value={userProfile.username} readOnly />
           </label>
-          <button type="submit">Guardar Cambios</button>
         </form>
       </div>
     )
