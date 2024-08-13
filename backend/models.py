e0876cc54e6727f0ee5d6ad40676316ae9916772
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-12 19:17:03 -0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -7,8 +7,10 @@ class Case(db.Model):
     fecha = db.Column(db.String(50))
     escritura = db.Column(db.Integer)
     radicado = db.Column(db.String(50))
-    protocolista = db.Column(db.String(50))
+    protocolista_id = db.Column(db.Integer, db.ForeignKey('protocolist.id'), nullable=False)
     observaciones = db.Column(db.String(255))
+
+    protocolista = db.relationship('Protocolist', backref=db.backref('cases', lazy=True))
 
     def to_dict(self):
         return {
@@ -16,20 +18,21 @@ class Case(db.Model):
             'fecha': self.fecha,
             'escritura': self.escritura,
             'radicado': self.radicado,
-            'protocolista': self.protocolista,
+            'protocolista': self.protocolista.nombre,
             'observaciones': self.observaciones
         }
 
 class Protocolist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50))
-    correo_electronico = db.Column(db.String(50))
+    nombre = db.Column(db.String(50), unique=True, nullable=False)
+    correo_electronico = db.Column(db.String(50), nullable=False)
 
     def to_dict(self):
         return {
             'id': self.id,
             'nombre': self.nombre,
-            'correo_electronico': self.correo_electronico
+            'correo_electronico': self.correo_electronico,
+            'numero_de_casos': len(self.cases)
         }
 
 class Radicado(db.Model):
