f3eb60c755136787f03b911878a2bb1c03886acf
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-16 22:57:51 -0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,27 @@
 import os
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://david:david1243@localhost/notaria15_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
+    UPLOAD_FOLDER = 'uploads'
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://david:david1243@localhost/notaria15_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = 'uploads'
+
+    # Configuración de Flask-Mail para Outlook
+    MAIL_SERVER = 'smtp-mail.outlook.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'drestrepo@correo.iue.edu.co')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'vera.96082719544')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'drestrepo@correo.iue.edu.co')
