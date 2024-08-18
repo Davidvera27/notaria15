d18ddce830656b7df7288f6fd3da92f48d9c6020
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-18 10:27:25 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_caching import Cache
 from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from models import db
 from config import Config
 from routes.cases import cases_bp
@@ -17,6 +19,9 @@ app.config.from_object(Config)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 db.init_app(app)
+
+# Integrando Flask-Migrate
+migrate = Migrate(app, db)
 
 # Registro de Blueprints para las rutas
 app.register_blueprint(cases_bp)
