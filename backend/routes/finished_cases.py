7761a569b84b3ef762b083dcd582be70de317eda
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-19 10:28:07 -0500

    Create finished_cases.py

---
+++ backend/routes/finished_cases.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify
+from models import CaseFinished  # Asegúrate de que el modelo esté correctamente definido
+
+finished_cases_bp = Blueprint('finished_cases_bp', __name__)
+
+@finished_cases_bp.route('/finished_cases', methods=['GET'])
+def get_finished_cases():
+    cases = CaseFinished.query.all()
+    result = [
+        {
+            'id': case.id,
+            'fecha': case.fecha,
+            'escritura': case.escritura,
+            'radicado': case.radicado,
+            'protocolista': case.protocolista,
+            'observaciones': case.observaciones
+        } for case in cases
+    ]
+    return jsonify(result)
