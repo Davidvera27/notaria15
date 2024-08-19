29eb7b28f273c71e62a2cd1dbe744c71a9260352
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-19 10:28:55 -0500

    Update Store.js

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import caseReducer from './features/caseSlice';
 import protocolistReducer from './features/protocolistSlice';
 import pdfDataReducer from './features/pdfDataSlice';
+import finishedCaseReducer from './features/finishedCaseSlice';
 
 
 const store = configureStore({
@@ -9,6 +10,7 @@ const store = configureStore({
     cases: caseReducer,
     protocolists: protocolistReducer,
     pdfData: pdfDataReducer,
+    finishedCases: finishedCaseReducer,
   },
 });
 
