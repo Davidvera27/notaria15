1dc99a65481a2b6184f77076a21c91f8dc459d03
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-18 23:46:23 -0500

    Update caseSlice.js

--- frontend/src/features/caseSlice.js
+++ frontend/src/features/caseSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+// Asynchronous thunk to fetch cases from the backend
 export const fetchCases = createAsyncThunk('cases/fetchCases', async () => {
   const response = await axios.get('http://127.0.0.1:5000/cases');
   return response.data;
@@ -13,7 +14,12 @@ const caseSlice = createSlice({
     status: 'idle',
     error: null
   },
-  reducers: {},
+  reducers: {
+    // Reducer to remove a case from the state
+    removeCase: (state, action) => {
+      state.cases = state.cases.filter(c => c.id !== action.payload);
+    }
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchCases.pending, (state) => {
@@ -29,5 +35,8 @@ const caseSlice = createSlice({
       });
   }
 });
+
+// Export the action to remove a case
+export const { removeCase } = caseSlice.actions;
 
 export default caseSlice.reducer;
