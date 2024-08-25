f237d1d704e2f59646f649b0193a7439eec2df30
Davidvera27 <davidrestrepovera@gmail.com>
2024-08-24 22:43:25 -0500

    Update Loader.js

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { TailSpin } from 'react-loader-spinner';
+import { Puff } from 'react-loader-spinner';  // Cambia TailSpin por Puff
 import './Loader.css';
 
 const Loader = () => {
   return (
     <div className="loader-container">
-      <TailSpin color="#007bff" height={80} width={80} />
+      <Puff color="#007bff" height={100} width={100} />
     </div>
   );
 };
