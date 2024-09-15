692c3b87b09a7d7712aa8e372007fe56fbcddda9
Davidvera27 <davidrestrepovera@gmail.com>
2024-09-14 23:56:46 -0500

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -2,39 +2,35 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useAuth0 } from '@auth0/auth0-react';
-import './Register.css'; // Importando el nuevo archivo de estilos
+import './Register.css';
 
 const Register = () => {
-  const { user, isAuthenticated } = useAuth0(); // Obtén los datos del usuario de Auth0
+  const { user, isAuthenticated } = useAuth0();
   const [form, setForm] = useState({
-    first_name1: '',
-    first_name2: '',
-    last_name1: '',
-    last_name2: '',
+    full_name: '',
+    last_name: '',
     phone_number: '',
     email: '',
     birth_date: '',
     username: '',
   });
 
-  // Función para prellenar los datos si el usuario está autenticado
   useEffect(() => {
     if (isAuthenticated && user) {
+      const [firstName, ...lastNameParts] = user.name.split(' ');
+      const lastName = lastNameParts.join(' ');
       setForm({
-        first_name1: user.given_name || '',
-        first_name2: '', // Asumimos que Auth0 no proporciona el segundo nombre directamente
-        last_name1: user.family_name || '',
-        last_name2: '', // Asumimos que Auth0 no proporciona el segundo apellido directamente
-        phone_number: '', // Asumimos que el número de teléfono no está disponible en Auth0
+        full_name: firstName || '',
+        last_name: lastName || '',
+        phone_number: '',
         email: user.email || '',
-        birth_date: '', // Asumimos que Auth0 no proporciona la fecha de nacimiento directamente
-        username: user.nickname || '', // Auth0 a veces usa 'nickname' como nombre de usuario
+        birth_date: '',
+        username: '',
       });
     }
   }, [isAuthenticated, user]);
 
   const handleChange = (e) => {
-    // Convertir a mayúsculas sostenidas si no es el campo de correo electrónico
     const value = e.target.name !== 'email' ? e.target.value.toUpperCase() : e.target.value;
     setForm({ ...form, [e.target.name]: value });
   };
@@ -42,43 +38,41 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://127.0.0.1:5000/userprofiles', form);
-      toast.success('Usuario creado exitosamente');
-      setForm({
-        first_name1: '',
-        first_name2: '',
-        last_name1: '',
-        last_name2: '',
-        phone_number: '',
-        email: '',
-        birth_date: '',
-        username: '',
-      });
+        if (!form.full_name || !form.last_name || !form.phone_number || !form.email || !form.birth_date) {
+            toast.error('Todos los campos son obligatorios.');
+            return;
+        }
+        
+        await axios.post('http://127.0.0.1:5000/userprofiles', form);
+        toast.success('Usuario creado exitosamente');
+        
+        // Reset form after successful submission
+        setForm({
+            full_name: '',
+            last_name: '',
+            phone_number: '',
+            email: '',
+            birth_date: '',
+            username: '',
+        });
     } catch (error) {
-      console.error('Error al crear el usuario:', error);
-      toast.error('Hubo un problema al crear el usuario. Por favor, inténtelo de nuevo más tarde.');
+        console.error('Error al crear el usuario:', error);
+        toast.error('Hubo un problema al crear el usuario. Por favor, inténtelo de nuevo más tarde.');
     }
-  };
+};
+
 
   return (
     <div className="register-container">
       <h2>Registrar Nuevo Usuario</h2>
       <form onSubmit={handleSubmit}>
         <label>
-          Primer Nombre:
-          <input type="text" name="first_name1" value={form.first_name1} onChange={handleChange} required />
+          Nombre completo:
+          <input type="text" name="full_name" value={form.full_name} onChange={handleChange} required />
         </label>
         <label>
-          Segundo Nombre:
-          <input type="text" name="first_name2" value={form.first_name2} onChange={handleChange} />
-        </label>
-        <label>
-          Primer Apellido:
-          <input type="text" name="last_name1" value={form.last_name1} onChange={handleChange} required />
-        </label>
-        <label>
-          Segundo Apellido:
-          <input type="text" name="last_name2" value={form.last_name2} onChange={handleChange} />
+          Apellido completo:
+          <input type="text" name="last_name" value={form.last_name} onChange={handleChange} required />
         </label>
         <label>
           Número telefónico:
@@ -91,10 +85,6 @@ const Register = () => {
         <label>
           Fecha de nacimiento:
           <input type="date" name="birth_date" value={form.birth_date} onChange={handleChange} />
-        </label>
-        <label>
-          Nombre de usuario:
-          <input type="text" name="username" value={form.username} onChange={handleChange} required />
         </label>
         <button type="submit">Crear Usuario</button>
       </form>
