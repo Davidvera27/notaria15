bd293409706c539a93125ec697404a427c9cec1b
Davidvera27 <davidrestrepovera@gmail.com>
2024-09-29 18:36:05 -0500

    Update file_monitor.py

--- backend/file_monitor.py
+++ backend/file_monitor.py
@@ -8,13 +8,28 @@ class FileHandler(FileSystemEventHandler):
     def __init__(self, folder_to_monitor):
         self.folder_to_monitor = folder_to_monitor
 
+    # Método para manejar cuando se crea un nuevo archivo en la carpeta
+    def on_created(self, event):
+        if event.src_path.endswith('.pdf'):
+            file_path = event.src_path
+            # Hacer la petición al servidor para procesar el archivo PDF
+            response = requests.post(f'http://127.0.0.1:5000/extract-data', json={'file_path': file_path})
+            if response.status_code == 200:
+                print(f"Processed {os.path.basename(file_path)}")
+            else:
+                print(f"Error processing {os.path.basename(file_path)}: {response.status_code}")
+
+    # Método para manejar cuando se modifica un archivo en la carpeta
     def on_modified(self, event):
         for filename in os.listdir(self.folder_to_monitor):
             if filename.endswith('.pdf'):
                 file_path = os.path.join(self.folder_to_monitor, filename)
+                # Hacer la petición al servidor para procesar el archivo PDF
                 response = requests.post(f'http://127.0.0.1:5000/extract-data', json={'file_path': file_path})
                 if response.status_code == 200:
                     print(f"Processed {filename}")
+                else:
+                    print(f"Error processing {filename}: {response.status_code}")
 
 if __name__ == "__main__":
     folder_to_monitor = "path/to/notaria15/backend/uploads"
